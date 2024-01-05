73a1305494a645a5f90673dc560137f7e713bb60
Hastingz666 <155504255+Hastingz666@users.noreply.github.com>
2024-01-05 16:12:47 +0200

    Update Gty.java

--- Gty.java
+++ Gty.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 public class Main {
   public static void main(String[] args) {
     HashSet<String> cars = new HashSet<String>();
-    cars.add("Volv2o");
+    cars.add("Vo8lv2o");
     cars.add("BMW");
     cars.add("Ford");
     cars.add("BMW");
