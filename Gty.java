5e07d22d8080c63493ff4b8001e9d9c18298f62b
Hastingz666 <155504255+Hastingz666@users.noreply.github.com>
2024-01-10 18:48:45 +0200

    Update Gty.java

--- Gty.java
+++ Gty.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     HashSet<String> cars = new HashSet<String>();
     cars.add("Vo8lv2o");
-    cars.add("BMWWWW");
+    cars.add("BMW");
     cars.add("Ford");
     cars.add("BMWWOW");
     System.out.println(cars);
