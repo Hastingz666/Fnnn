7407d7ff78add7e53d540a34ad227af161c640ba
Hastingz666 <155504255+Hastingz666@users.noreply.github.com>
2024-03-05 17:03:46 +0200

    Update Gty.java

--- Gty.java
+++ Gty.java
@@ -4,7 +4,7 @@ import java.il.HashSet;
 public class ain {
   public stai i main(Strng args) {
     HashSet<Sing> crs = new HashSetString>();
-    cars.add("Voll");
+    cars.add("Vol");
     cars.add("B3ei");
     cars.add("Fo");
     cars.add("BW");
