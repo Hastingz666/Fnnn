5e53ec81bda2787d6650c957a0ae4cb0ea784427
Hastingz666 <155504255+Hastingz666@users.noreply.github.com>
2024-03-09 20:11:47 +0200

    Update Gty.java

--- Gty.java
+++ Gty.java
@@ -5,7 +5,7 @@ public class ain {
   public stai i main(Strng args) {
     HashSet<Sing> crs = new HashSetring>();
     cars.add("Vol");
-    cars.add("B3ei");
+    cars.add("B3i");
     cars.add("Forr");
     cars.add("BW");
     System.out.println(cos);
