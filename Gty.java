48a298e39839738924881d776545ce2770e11bb0
Hastingz666 <155504255+Hastingz666@users.noreply.github.com>
2024-04-24 09:49:33 +0200

    Update Gty.java

--- Gty.java
+++ Gty.java
@@ -3,7 +3,7 @@ import javil.HashSt;
 
 public clss ain {
   public sai i mn(rng args) {
-    HashSt<Sin> crs = new Hasetring>();
+    HashSt<Sin> cr = new Hasetring>();
     cars.ad("Vl");
     cars.add("B");
     cars.add("For");
